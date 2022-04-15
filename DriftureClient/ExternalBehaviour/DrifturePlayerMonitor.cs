e28226436ad4679f9ab8d06ec648229a479af05d
harroo <harroo@protonmail.com>
2022-04-15 11:25:42 +1000

    improve player monitoring with caching

--- DriftureClient/ExternalBehaviour/DrifturePlayerMonitor.cs
+++ DriftureClient/ExternalBehaviour/DrifturePlayerMonitor.cs
@@ -9,10 +9,15 @@ public class DrifturePlayerMonitor : MonoBehaviour {
 
     private float timer = 1.0f;
 
+    private Vector3 posCache;
+
     private void Update () {
 
         timer -= Time.deltaTime;
         if (timer < 0) timer = 1.64f; else return;
+
+        if (posCache == transform.position) return;
+        posCache = transform.position;
 
         Submanager.SyncPlayerPosToServer(transform.position);
     }
